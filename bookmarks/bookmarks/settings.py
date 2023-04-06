09d53b937caec014a71d6f322b581166db1d6e9a
Kuma Original <Kuma_Original@gmail.com>
2023-04-06 17:14:36 +0600

    Использование системы аутентификации Django

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -118,8 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_REDIRECT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
